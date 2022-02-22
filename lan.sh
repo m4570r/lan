b660973c3004632083c8bf6d38275f4f9537cb59
m4570r <91635371+m4570r@users.noreply.github.com>
2022-02-22 15:17:48 -0300

    Update lan.sh

--- lan.sh
+++ lan.sh
@@ -4,12 +4,12 @@ do
 #if ping -c1 google.com &>/dev/null; then  
   	echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
 		if [ $? -eq 0 ]; then
-    			ip -o addr show up primary scope global | while read -r num dev fam addr rest; do echo ${addr%/*}; done>~/.config/polybar/scripts/lan/lan.view
+    			ip -o addr show up primary scope global | while read -r num dev fam addr rest; do echo ${addr%/*}; done>~/lan/lan.view
 		else
-    			echo "Desconectado">~/.config/polybar/scripts/lan/lan.view
+    			echo "Desconectado">~/lan/lan.view
 		fi
 #else 
-#	echo 'Desconectado'>~/.config/polybar/scripts/lan/lan.view
+#	echo 'Desconectado'>~/lan/lan.view
 #fi
 sleep 1s
 done
