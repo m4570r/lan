feff5c2575479bcca25d2d9471605d171579aa70
m4570r <91635371+m4570r@users.noreply.github.com>
2022-02-24 16:35:50 -0300

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-usuario=
+users>usuarios
+while IFS= read -r line; do usuario=$line; done < usuarios
 for (( ; ; ))
 do
 IFACE=$(/usr/sbin/ifconfig | grep tun0 | awk '{print $1}' | tr -d ':')
