42a5d8d93ebb5686cc8836119137de74aade7d6e
m4570r <91635371+m4570r@users.noreply.github.com>
2022-02-24 18:45:35 -0300

    Update wan.sh

--- wan.sh
+++ wan.sh
@@ -8,8 +8,8 @@ echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
 if [ $? -eq 0 ]; then
    dig +short myip.opendns.com @resolver1.opendns.com>/home/$usuario/.config/polybar/scripts/lan/wan.view
 else
-      cd /home/$usuario/.config/polybar/scripts/lan
-    	echo "Desconectado">wan.view
+      #cd /home/$usuario/.config/polybar/scripts/lan
+    	echo "Desconectado">/home/$usuario/.config/polybar/scripts/lan/wan.view
 fi
 #else 	
 #	echo "Desconectado">wan.view
