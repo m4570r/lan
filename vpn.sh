8bfaa78d456b0fe388894d4071fedd4e5af04799
m4570r <91635371+m4570r@users.noreply.github.com>
2022-02-25 00:05:04 -0300

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-users>~/lan/usuarios
-while IFS= read -r line; do usuario=$line; done<~/lan/usuarios
+users>usuarios ; while IFS= read -r line; do usuario=$line; done < usuarios
 for (( ; ; ))
 do
 IFACE=$(/usr/sbin/ifconfig | grep tun0 | awk '{print $1}' | tr -d ':')
 if [ "$IFACE" = "tun0" ]; then
     	echo "$(/usr/sbin/ifconfig tun0 | grep "inet " | awk '{print $2}')">/home/$usuario/.config/polybar/scripts/lan/vpn.view
 else
-      #cd /home/$usuario/.config/polybar/scripts/lan
-    	echo "Desconectado">/home/$usuario/.config/polybar/scripts/lan/vpn.view
+      cd /home/$usuario/.config/polybar/scripts/lan
+      echo "Desconectado">vpn.view
 fi
 sleep 1s
 done
